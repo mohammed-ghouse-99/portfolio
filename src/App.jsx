4439e887a858072957ff811b9aa30dee03a58f59
md-ghouse-107 <ghousenaser2005@gmail.com>
2025-11-12 01:23:17 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Portfolio from "./Portfolio";
+import Portfolio from "./index.jsx";
 import ResearchPapers from "./ResearchPapers.jsx";
 
 
